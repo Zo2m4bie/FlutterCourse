38bc2d80810377bea68dce2c1d797826fb6a7900
Zo2m4bie <kovalenkodwork@gmail.com>
2019-02-26 07:59:46 +0200

    continue course

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'dart:async';
-
+import 'dart:io';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:rxdart/subjects.dart';
+import 'package:mime/mime.dart';
+import 'package:http_parser/http_parser.dart';
 
 import '../models/product.dart';
 import '../models/user.dart';
@@ -57,16 +59,57 @@ mixin ProductsModel on ConnectedProductsModel {
     return _showFavorites;
   }
 
-  Future<bool> addProduct(String title, String description, String image,
+  Future<Map<String, dynamic>> uploadImage(File image,
+      {String imagePath}) async {
+    final mimeTypeData = lookupMimeType(image.path).split('/');
+    final imageUploadRequest = http.MultipartRequest(
+        'POST',
+        Uri.parse(
+            'https://us-central1-test-d9e23.cloudfunctions.net/storeImage'));
+    final file = await http.MultipartFile.fromPath(
+      'image',
+      image.path,
+      contentType: MediaType(mimeTypeData[0], mimeTypeData[1]),
+    );
+    imageUploadRequest.files.add(file);
+    if(imagePath != null) {
+      imageUploadRequest.fields['imagePath'] = Uri.encodeComponent(imagePath);
+    }
+    imageUploadRequest.headers['Authorization'] = 'Bearer ${_authenticatedUser.token}';
+    try{
+      final streamedResponse = await imageUploadRequest.send();
+      final response = await http.Response.fromStream(streamedResponse);
+      if(response.statusCode != 200 && response.statusCode != 201){
+        print('Somthing wentWorng');
+        print(response.body);
+        print(response.statusCode);
+        return null;
+      }
+      final responseDate = json.decode(response.body);
+      return responseDate;
+    }catch(error){
+      print(error);
+      return null;
+    }
+  }
+
+  Future<bool> addProduct(String title, String description, File image,
       double price, LocationData locationData) async {
     _isLoading = true;
     notifyListeners();
+    final uploadData = await uploadImage(image);
+    if(uploadData == null) {
+      print('Upload failed');
+      return false;
+    }
     final Map<String, dynamic> productData = {
       'title': title,
       'description': description,
       'image':
           'https://upload.wikimedia.org/wikipedia/commons/6/68/Chocolatebrownie.JPG',
       'price': price,
+      'imagePath': uploadData['imagePath'],
+      'imageUrl': uploadData['imageUrl'],
       'userEmail': _authenticatedUser.email,
       'userId': _authenticatedUser.id,
       'loc_lat': locationData.latitude,
@@ -89,7 +132,7 @@ mixin ProductsModel on ConnectedProductsModel {
           id: responseData['name'],
           title: title,
           description: description,
-          image: image,
+          image: uploadData['imageUrl'],
           price: price,
           userEmail: _authenticatedUser.email,
           userId: _authenticatedUser.id,
@@ -110,8 +153,8 @@ mixin ProductsModel on ConnectedProductsModel {
     // });
   }
 
-  Future<bool> updateProduct(
-      String title, String description, String image, double price, LocationData locationData ) {
+  Future<bool> updateProduct(String title, String description, String image,
+      double price, LocationData locationData) {
     _isLoading = true;
     notifyListeners();
     final Map<String, dynamic> updateData = {
